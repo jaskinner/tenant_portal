50779774c3b4b77a2012a91f06070498cee95e5f
Jon Skinner <jon.a.skinner1@gmail.com>
2023-10-23 17:04:09 -0400

    starting front end scaffolding adding axios

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import instance from './axios'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +11,7 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.provide('$axios', instance);
 
 app.mount('#app')
