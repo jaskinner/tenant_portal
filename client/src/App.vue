e76ae249834dbef3e8e9271b68f2656b8de57203
Jon Skinner <jon.a.skinner1@gmail.com>
2023-11-01 21:05:28 -0400

    auth work login and logout added refactoring auth functions into auth store showing and hiding login form conditionally removing login route removing login view adding profile view stub

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,13 @@
 <script setup>
 import NavbarComponent from './components/NavbarComponent.vue'
+import { inject } from 'vue'
+
+const $axios = inject('$axios')
+const token = localStorage.getItem('auth_token')
+
+if (token) {
+	$axios.defaults.headers.common['Authorization'] = `Bearer ${token}`
+}
 </script>
 <template>
 	<NavbarComponent></NavbarComponent>
--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -3,14 +3,39 @@ import { defineStore } from "pinia";
 export const useAuthStore = defineStore({
 	id: 'auth',
 	state: () => ({
-		token: null
+		token: localStorage.getItem('auth_token') || ''
 	}),
+	getters: {
+		isLoggedIn: (state) => !!state.token
+	},
 	actions: {
 		setToken(token) {
 			this.token = token;
+			localStorage.setItem('auth_token', token);
 		},
-		clearToken() {
-			this.token = null;
+		removeToken() {
+			this.token = '';
+			localStorage.removeItem('auth_token');
+		},
+		async login($axios, username, password) {
+
+			try {
+				const { data } = await $axios.post('/api/auth/login', {
+					username: username,
+					password: password
+				})
+
+				if (data && data.token) {
+					this.setToken(data.token);
+				}
+
+				$axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`
+			} catch (error) {
+				console.log(error)
+			}
+		},
+		logout() {
+			this.removeToken();
 		}
 	}
-})
+});
